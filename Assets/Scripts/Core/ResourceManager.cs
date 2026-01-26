5bad2df03b8f6231b0fa69b638d738099c56792f
Christoph Leitgeb <133106478+alapple@users.noreply.github.com>
2026-01-26 16:44:47 +0100

    Time Manager (#36)
    * add TimeManager class for calculating months and years.
    
    * add ResourceManager listening on TimeManager for year change and adding random Resources.
    
    * add Action to listen to changed Resources

--- Assets/Scripts/Core/ResourceManager.cs
+++ Assets/Scripts/Core/ResourceManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 using Resource = Data.Resources.Resources;
 
 namespace Core
@@ -7,15 +9,22 @@ namespace Core
     public class ResourceManager : MonoBehaviour
     {
         public List<Resource> resources = new();
-        
+
         private readonly Dictionary<Resource, int> _resourcesAmount = new();
+
+        public event Action<Resource, int> OnResourceChanged;
 
         void Awake()
         {
-            AddRandomResource();
+            GenerateRandomResources();
         }
 
-        private void AddRandomResource()
+        void Start()
+        {
+            TimeManager.Instance.OnYearChanged += AddRandomResources;
+        }
+
+        private void GenerateRandomResources()
         {
             foreach (Resource res in resources)
             {
@@ -23,15 +32,26 @@ namespace Core
                 Debug.Log($"Added {res} with amount {_resourcesAmount[res]}");
             }
         }
-        
+
+        private void AddRandomResources(int year)
+        {
+            foreach (var resource in new List<Resource>(_resourcesAmount.Keys))
+            {
+                _resourcesAmount[resource] += Random.Range(resource.minValue, resource.maxValue);
+                OnResourceChanged?.Invoke(resource, _resourcesAmount[resource]);
+            }
+        }
+
         public void AddResource(Resource resource, int amount)
         {
             _resourcesAmount[resource] += amount;
+            OnResourceChanged?.Invoke(resource, _resourcesAmount[resource]);
         }
 
         public void RemoveResourceU(Resource resource, int amount)
         {
             _resourcesAmount[resource] -= amount;
+            OnResourceChanged?.Invoke(resource, _resourcesAmount[resource]);
         }
     }
 }
