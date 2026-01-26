cc2e37f065c9c30dd522cb24d86c93b567e58b3c
Crisyn <125381193+Crisyn@users.noreply.github.com>
2026-01-26 16:50:03 +0100

    Feature/16 add quest controller (#37)
    * add QuestController
    
    add dummy randomizer classe and ui
    
    Signed-off-by: Crisyn <jakob.juro@gmail.com>
    
    * add conceptual ResourceManager
    
    Signed-off-by: Crisyn <jakob.juro@gmail.com>
    
    * add ResourceUIManager.cs
    
    chage ResourceManager.cs (Core)
    
    Signed-off-by: Crisyn <jakob.juro@gmail.com>
    
    ---------
    
    Signed-off-by: Crisyn <jakob.juro@gmail.com>

---
+++ Assets/Scripts/Core/ResourceRandomizer.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+public class ResourceRandomizer
+{
+    //TODO: Make actual randomizer. Has to return the SortedDictionary<string, int>
+    //this is just for testing purposes. look into the ResourceManager class for real namings
+    public Dictionary<string, int> ReturnResources()
+    {
+        Dictionary<string, int> resources = new Dictionary<string, int>();
+
+        resources.Add("Potato", 10);
+        resources.Add("Vodka", 10);
+        resources.Add("Megafon", 10);
+        resources.Add("Seal", 10);
+        resources.Add("Repair Materials", 10);
+        resources.Add("Workers", 10);
+
+        return resources;
+    }
+}
---
+++ Assets/Scripts/UI/Controllers/ResourceUIManager.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using Core;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Resource = Data.Resources.Resources;
+
+/*
+ * Manage the resource bar and the potato amount display of the quest
+ */
+
+//TODO: Change once ui is ready
+public class ResourceUIManager : MonoBehaviour
+{
+    [SerializeField] private UIDocument uiDocument;
+    [SerializeField] private ResourceManager resourceManager;
+
+    private VisualElement _root;
+
+    private List<Label> _resourceLabels = new List<Label>();
+
+    private QuestAmountRandomizer _questAmountRandomizer;
+
+    void Awake()
+    {
+        uiDocument = GetComponent<UIDocument>();
+        _resourceLabels = uiDocument.rootVisualElement.Query<Label>().ToList();
+
+        var amounts = resourceManager.GetResourcesAmount();
+        SetupResourcesUI(amounts);
+    }
+
+    //The name of the ScriptableObject name must match the Labels name
+    private void SetupResourcesUI(Dictionary<Resource, int> resources)
+    {
+        foreach (var resourceLabel in _resourceLabels)
+        {
+            foreach (var resource in resources)
+            {
+                if (resourceLabel.name == resource.Key.resourceName)
+                {
+                    resourceLabel.text = resource.Value.ToString();
+                }
+            }
+        }
+    }
+
+    public void ChangeResources(string uiTreeName, int amount)
+    {
+        var resources = resourceManager.resources;
+        Resource resourceChanged = null;
+        foreach (var resource in resources)
+        {
+            if (resource.name == uiTreeName)
+            {
+                Debug.Log(resource.name);
+                resourceChanged = resource;
+            }
+        }
+
+        foreach (var resourceLabel in _resourceLabels)
+        {
+            if (resourceLabel.name == uiTreeName)
+            {
+                resourceLabel.text = (Int32.Parse(resourceLabel.text) + amount).ToString();
+                resourceManager.AddResource(resourceChanged, amount);
+            }
+        }
+    }
+}
