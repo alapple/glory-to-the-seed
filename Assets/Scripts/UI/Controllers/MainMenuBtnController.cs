87f84be4e8fd78c2c2e5dc4e78b15ebde4703efc
Christoph Leitgeb <133106478+alapple@users.noreply.github.com>
2026-01-27 11:33:34 +0100

    TimeManager Starts Counting on OnGameStart Event (#59)

--- Assets/Scripts/UI/Controllers/MainMenuBtnController.cs
+++ Assets/Scripts/UI/Controllers/MainMenuBtnController.cs
@@ -7,14 +7,16 @@ namespace UI.Controllers
 {
     public class MainMenuBtnController : MonoBehaviour
     {
+        public static MainMenuBtnController Instance;
         [SerializeField] private UIDocument _mainMenuDocument;
 
         private UIDocument _document;
 
-        private event Action OnGameStart;
+        public event Action OnGameStart;
 
         void Awake()
         {
+            Instance = this;
             var startBtn = _mainMenuDocument.rootVisualElement.Q<Button>("StartButton");
             var creditsBtn = _mainMenuDocument.rootVisualElement.Q<Button>("CreditButton");
             var quitBtn = _mainMenuDocument.rootVisualElement.Q<Button>("QuitButton");
