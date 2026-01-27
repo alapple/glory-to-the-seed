aeac2750456a55a63f58e14dc6ec22fc2781aa6a
Crisyn <125381193+Crisyn@users.noreply.github.com>
2026-01-27 16:49:07 +0100

    Feature/78 add select field (#82)
    * select field by leftclicking
    
    Signed-off-by: Crisyn <jakob.juro@gmail.com>
    
    * remove log
    
    Signed-off-by: Crisyn <jakob.juro@gmail.com>
    
    ---------
    
    Signed-off-by: Crisyn <jakob.juro@gmail.com>

---
+++ Assets/Scripts/UI/Controllers/SelectFieldController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace UI.Controllers
+{
+    public class SelectFieldController : MonoBehaviour
+    {
+        private PolygonCollider2D _field;
+
+        void Start()
+        {
+            _field = GetComponent<PolygonCollider2D>();
+        }
+
+        void Update()
+        {
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                Vector3 mousePos = Mouse.current.position.ReadValue();
+
+                Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+                Vector2 mousePos2D = new Vector2(worldPos.x, worldPos.y);
+
+                if (_field.OverlapPoint(mousePos2D))
+                {
+                }
+            }
+        }
+    }
+}
